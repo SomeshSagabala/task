94836cc730778f7c31af75ffc6e30d5e8a85784d
careerpedia <somesh.11careerpedia>
2024-05-19 10:21:50 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import data from './product.json';
+
+function App() {
+
+  const data = {
+    name : "somesh"
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <div>Name : {data.name}</div>
+    </div>
+  );
+
+}
+
+
+
+
+export default App;
